055f7f594a275e4ccc9f123e9c2bc9696bd313d7
kakashimaker <87550718+K4K4SH1M4K3R@users.noreply.github.com>
2021-09-02 16:05:58 -0300

    Delete Script.py

--- Script.py
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env python      
-import sys
-print ""+N+"       =[ "+O+"LOS HERMANOS CURSOS"+N+"        >print "+ -- --=[ LOS DA PIROCA GRANDE"
-print "+ -- --=[ Blog meu ai fds https://onerswerswebprivada.blogspot.com/?m=1 "+N+
-print "OI MEU NOME É KAKASHI E ESTOU FAZENDO UM TIPO DE ESCOLHA DE PROGRAMAÇÃO"
-print "bom primeiro vou deixar dois links de sites de programação e hacking" 
-print "https://jornadadodev.com.br/cursos    &    https://onerswerswebprivada.blogspot.com/?m=1"
-print "e com isso vcs vão cópia e colar no Google próxima aba vamos dar estudos de termux"
