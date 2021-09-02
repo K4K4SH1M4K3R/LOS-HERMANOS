0702c8d5cb4e512f207ab389f6cd39c734070259
kakashimaker <87550718+K4K4SH1M4K3R@users.noreply.github.com>
2021-09-02 15:55:04 -0300

    Create Script.py

---
+++ Script.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python      
+import sys
+import os as sistema                                            
+# Set color                                                     
+R = '\033[31m' # Red                                           
+ N = '\033[1;37m' # White
+os.system("cd banner;python2 banner4.py")                       print                                                           print
+print ""+N+"       =[ "+O+"LOS HERMANOS CURSOS"+N+"        >print "+ -- --=[ LOS DA PIROCA GRANDE"
+print "+ -- --=[ Blog meu ai fds https://onerswerswebprivada.blogspot.com/?m=1 "+N+
+print "OI MEU NOME É KAKASHI E ESTOU FAZENDO UM TIPO DE ESCOLHA DE PROGRAMAÇÃO"
+print "bom primeiro vou deixar dois links de sites de programação e hacking" 
+print "https://jornadadodev.com.br/cursos    &    https://onerswerswebprivada.blogspot.com/?m=1"
+print "e com isso vcs vão cópia e colar no Google próxima aba vamos dar estudos de termux"
